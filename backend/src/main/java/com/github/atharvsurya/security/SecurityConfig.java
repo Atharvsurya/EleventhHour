d36c4dd1373471a509ac074c93676e06c19b6fd2
Atharvsurya <atharvsurya007@gmail.com>
2026-06-25 19:03:39 +0530

    Authentication and MultiUser support added

--- backend/src/main/java/com/github/atharvsurya/security/SecurityConfig.java
+++ backend/src/main/java/com/github/atharvsurya/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**","/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
