6bb12027d6e12a1e8529d2d88c5a84f64244db31
Atharvsurya <atharvsurya007@gmail.com>
2026-06-26 12:05:08 +0530

    TaskView.jsx updated

--- backend/src/main/resources/static/frontend/src/views/TasksView.jsx
+++ backend/src/main/resources/static/frontend/src/views/TasksView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TaskCard from "../components/TaskCard";
 import TaskModal from "../components/TaskModal";
 
